e663e1a45741733547e7983cdddbfd79c09588c0
Yuriy Cherniy <amidobox@gmail.com>
2021-06-10 12:26:10 +0300

    commit

--- app_to_dockerize/app_to_dockerize/settings.py
+++ app_to_dockerize/app_to_dockerize/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
